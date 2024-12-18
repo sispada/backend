4a1b2d5a6aabc8737f253eb260c5caa5d0ea476a
monoland <mono.socialmedia@gmail.com>
2024-12-18 12:39:38 +0700

    update

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
         'platform' => [
             'driver' => 'pgsql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '127.0.0.3'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
